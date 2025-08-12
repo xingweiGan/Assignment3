e2ed25dd1af3b76f6575277571535ac667ce204c
XW <xigan@ucsd.edu>
2025-08-12 06:03:46 +0000

    Describe your change here

---
+++ cs336_alignment/sft.experiment.py
@@ -0,0 +1,39 @@
+
+#0. Load the pretrained model-Qwen
+from vllm.model_executor import set_random_seed as vllm_set_random_seed
+
+def init_vllm(model_id: str, device: str, seed: int, gpu_memory_utilization: float = 0.85):
+    """
+    Start the inference process, here we use vLLM to hold a model on
+    a GPU separate from the policy.
+    """
+    vllm_set_random_seed(seed)
+
+    # Monkeypatch from TRL:
+    # Patch vLLM to make sure we can
+    # (1) place the vLLM model on the desired device (world_size_patch) and
+    # (2) avoid a test that is not designed for our setting (profiling_patch).
+    world_size_patch = patch("torch.distributed.get_world_size", return_value=1)
+    profiling_patch = patch(
+        "vllm.worker.worker.Worker._assert_memory_footprint_increased_during_profiling",
+        return_value=None
+    )
+    with world_size_patch, profiling_patch:
+        return LLM(
+            model=model_id,
+            device=device,
+            dtype=torch.bfloat16,
+            enable_prefix_caching=True,
+            gpu_memory_utilization=gpu_memory_utilization,
+        )
+
+def load_policy_into_vllm_instance(policy: PreTrainedModel, llm: LLM):
+    state_dict = policy.state_dict()
+    llm_model = llm.llm_engine.model_executor.driver_worker.model
+    llm_model.load_weights(state_dict.items())
+
+#1. Tokenize SFT dataset- Here we use the original form 
+
+#2. Compute the loss and gradient of a batch sampled from SFT dataset AND on test.jsonl to get validation accuracy (Use R1-format)
+
+#3. Update parameters
